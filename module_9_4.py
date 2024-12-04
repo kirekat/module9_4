bda089853d073e25f1843c8ce604d154170f1347
kirekat <kec85@mail.ru>
2024-12-04 20:45:56 +0300

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,33 @@
+# 1. Lambda-функция:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+# 2. Замыкание
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a') as file:
+            for i in data_set:
+                file.write(f'{str(i)}\n')
+
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# 3. Метод __call__
+from random import choice
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
